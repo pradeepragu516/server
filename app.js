5ea7f7fc22935791ab309d0778bf27a2bb8663f9
pradeepragu516 <pradeep.m2023it@sece.ac.in>
2025-01-25 11:55:59 +0530

    added the authentication

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ app.use(express.json())//parsing the data
 const mongoose = require("mongoose")
 const { v4: uuidv4 } = require('uuid');
 const PORT= 8000;
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 
     //  const students=[
@@ -58,17 +60,17 @@ const expenseSchema = new mongoose.Schema({
 const expenseModel = mongoose.model("expense-tracker", expenseSchema);// collection name, schema name
 
 
-app.post("/api/expenses", async (req, res) => {
-    const { title, amount} = req.body;
-    const newExpense = new expenseModel({
-        id: uuidv4(),
-        title: title,
-        amount: amount
-    });
+// app.post("/api/expenses", async (req, res) => {
+//     const { title, amount} = req.body;
+//     const newExpense = new expenseModel({
+//         id: uuidv4(),
+//         title: title,
+//         amount: amount
+//     });
 
-    const savedExpense = await newExpense.save();
-    res.status(200).json(savedExpense);
-});
+//     const savedExpense = await newExpense.save();
+//     res.status(200).json(savedExpense);
+// });
 
 //getby id
 app.get("/api/expenses/:id", async (req, res) => {
@@ -117,9 +119,96 @@ app.delete("/api/expenses/:id", async (req, res) => {
 app.delete("/api/expense",async(req,res)=>{
     const deleteAll = await expenseModel.deleteMany();
     res.status(200).json(deleteAll);
+});
+
+/Authentication/
+
+//Schema
+const userSchema = new mongoose.Schema({
+    username: {type:String,required:true,unique:true},
+    password:{type:String,required:true},
+    });
+
+//Model
+const User = mongoose.model("User",userSchema);
+
+//Register api
+app.post("/api/user/register",async(req,res)=>{
+    const {username,password} = req.body;
+//Validation
+    if(!username || !password){
+        return res.status(400).json({message:"Username is required"});
+    }
+//Check if user already exists
+    const ExsistingUser = await User.find({username});
+    if(!ExsistingUser){
+        return res.status(400).json({message:"User already exists"});
+    }
+//Hash the password
+    const hashedpass = await bcrypt.hash(password,8);
+
+//Create new user
+    const newUser = new User({
+        username,
+        password:hashedpass,
+    })
+
+    await newUser.save();
+
+    return res.status(200).json({message:"User registered successfully"});
 })
 
-const mongourl = "mongodb+srv://pradeepragu:Pradeep.ragu16@cluster0.15ndn.mongodb.net/test"
+//Login api
+app.post("/api/user/login",async(req,res)=>{
+    const {username,password} = req.body;
+//Validation
+    if(!username || !password){
+        return res.status(400).json({message:"Username and password is required"});
+    }
+
+//Check if user exists
+    const user = await User.findOne({username});
+    if(!user){
+        return res.status(400).json({message:"User not found"});
+    }
+//Check if password is correct
+    const isPasswordMatch = await bcrypt.compare(password,user.password);
+    
+    if(!isPasswordMatch){
+        return res.status(400).json({message:"Invalid credentials"});
+    }
+
+    const secret = "learn_nodejs";
+    const token = jwt.sign({username},secret,{expiresIn:"1h"});
+
+    return res.status(200).json(
+        {message:"Login successful",
+        token:token,
+        }
+    );
+    
+})
+
+//middleware process
+function authenticateToken(req, res, next) {
+    const token = req.header("Authorization")?.split(" ")[1];
+  
+    if (!token) return res.status(401);
+  
+    jwt.verify(token, "learn_nodejs", (err, user) => {
+      if (err) return res.sendStatus(403);
+      req.user = user;
+      next();
+    });
+  }
+  
+  app.get("/api/user/getAll", authenticateToken, async (req, res) => {
+    const users = await User.find();
+    res.status(200).json(users);
+  });
+
+
+const mongourl = "your url"
 mongoose.connect(mongourl) .then(() => {
     console.log("mongodb connected")
     app.listen(PORT, () => {
